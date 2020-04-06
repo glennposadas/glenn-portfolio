ffa8c24ff2ca61c13a4fd7f51049f96d8e5e2def
Glenn Posadas <hello@glennvon.com>
2020-04-07 00:31:50 +0800

    add boilerplates.

---
+++ portfolio/public/js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDNSodbr4EjHXLNRvySMUaBqiw6aOv_y5M",
+  authDomain: "glenn-portfolio.firebaseapp.com",
+  databaseURL: "https://glenn-portfolio.firebaseio.com",
+  projectId: "glenn-portfolio",
+  storageBucket: "glenn-portfolio.appspot.com",
+  messagingSenderId: "219450711315",
+  appId: "1:219450711315:web:c935872d01eff35809cee6",
+  measurementId: "G-83ZYEL4H6F"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
