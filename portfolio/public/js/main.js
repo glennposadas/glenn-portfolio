6caa2f9fea860260789e273ea913280d1feb9ce8
Glenn Posadas <hello@glennvon.com>
2020-04-07 19:05:38 +0800

    add download resume button.

---
+++ portfolio/public/js/main.js
@@ -0,0 +1,14 @@
+
+const downloadResumeButton = document.getElementById('download-resume-button')
+
+downloadResumeButton.addEventListener('click', downloadResume)
+
+function downloadResume() {
+  open('https://firebasestorage.googleapis.com/v0/b/glenn-portfolio.appspot.com/o/POSADAS_GLENN_VON_C_RESUME_MARCH_2020.pdf?alt=media&token=81637c4f-d9f7-4397-980a-531ec51e6203', '_blank')
+
+  const event = new Date();
+  const dateISO = event.toISOString()
+  analytics.logEvent('download_resume', { date: dateISO});
+
+  console.log('Download!!!')
+}
