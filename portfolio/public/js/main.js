740d1604123d273b2abe4135402d5a4efc1e003b
Glenn Posadas <hello@glennvon.com>
2020-04-07 21:57:40 +0800

    fix header layout for iPhone 8 and similar height devices.

--- portfolio/public/js/main.js
+++ portfolio/public/js/main.js
@@ -1,8 +1,9 @@
 const defaultAnalytics = firebase.analytics()
 
-const downloadResumeButton = document.getElementById('download-resume-button')
+const buttonDownloadResume = document.getElementById('download-resume-button')
+const linkEmail = document.getElementById('')
 
-downloadResumeButton.addEventListener('click', downloadResume)
+buttonDownloadResume.addEventListener('click', downloadResume)
 
 function downloadResume() {
   open('https://firebasestorage.googleapis.com/v0/b/glenn-portfolio.appspot.com/o/POSADAS_GLENN_VON_C_RESUME_MARCH_2020.pdf?alt=media&token=81637c4f-d9f7-4397-980a-531ec51e6203', '_blank')
