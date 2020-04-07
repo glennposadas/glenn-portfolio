94d74243af81045da1763d6a0a0766d4d1eaba74
Glenn Posadas <hello@glennvon.com>
2020-04-07 19:20:59 +0800

    add download resume event.

--- portfolio/public/js/main.js
+++ portfolio/public/js/main.js
@@ -1,3 +1,4 @@
+const defaultAnalytics = firebase.analytics()
 
 const downloadResumeButton = document.getElementById('download-resume-button')
 
@@ -6,9 +7,7 @@ downloadResumeButton.addEventListener('click', downloadResume)
 function downloadResume() {
   open('https://firebasestorage.googleapis.com/v0/b/glenn-portfolio.appspot.com/o/POSADAS_GLENN_VON_C_RESUME_MARCH_2020.pdf?alt=media&token=81637c4f-d9f7-4397-980a-531ec51e6203', '_blank')
 
-  const event = new Date();
+  const event = new Date()
   const dateISO = event.toISOString()
-  analytics.logEvent('download_resume', { date: dateISO});
-
-  console.log('Download!!!')
+  defaultAnalytics.logEvent('download_resume', { date: dateISO})
 }
