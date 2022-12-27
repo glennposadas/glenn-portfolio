bd672e6c9483c45d2b206bde9869f25d38db29a6
Glenn Posadas <hello@glennvon.com>
2022-12-27 10:10:06 +0800

    update direct resume link.

--- portfolio/public/js/main.js
+++ portfolio/public/js/main.js
@@ -10,7 +10,9 @@ buttonViewProjects.addEventListener('click', viewProjects)
 function downloadResume() {
   // Now we have an editable url :P No need to update this js file
   // everytime we have updates to my resume.
-  open('https://rebrand.ly/glennposadas/resume')
+  // As of 12/27/22, my rebrandly link got blocked after updating my resume file. I've requested to unblock it though.
+  // For now, this is the direct link of my resume file.
+  open('https://firebasestorage.googleapis.com/v0/b/glenn-portfolio.appspot.com/o/POSADAS_GLENN_VON_C_2023.pdf?alt=media&token=4f50cfce-75f4-4581-be95-1135008fa2ba')
 
   const event = new Date()
   const dateISO = event.toISOString()
